afc32533950a1c7f20516b0ecfddc89c38978459
Argaaa63 <argaaaa53@gmail.com>
2024-10-13 15:20:58 +0700

    login design

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,21 @@
 @extends('template.layout')
 @section('konten')
-    <div class="w-full h-screen flex">
-        <div class="login">
-            <form action="">
-
+    <div class="w-full h-screen bg-gray-100 flex justify-center items-center">
+        <div class="login p-8 bg-white rounded-lg shadow-md">
+            <h1 class="text-4xl font-bold text-center mb-4">login</h1>
+            <form action="" class="flex flex-col gap-4">
+                <label for="username" class="block text-xl font-medium text-gray-700">username</label>
+                <input type="text" name="username" id="username"
+                    class="block w-full p-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+                <label for="password" class="block text-xl font-medium text-gray-700">password</label>
+                <input type="password" name="password" id="password"
+                    class="block w-full p-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required>
+                <div class="btn flex gap-2">
+                    <button type="submit"
+                        class="w-full bg-blue-600 text-white font-bold p-2 rounded-md hover:bg-blue-700">login</button>
+                </div>
             </form>
         </div>
     </div>
--- resources/views/pelanggan/pelanggan-edit.blade.php
+++ resources/views/pelanggan/pelanggan-edit.blade.php
@@ -14,7 +14,7 @@
                 <div class="mb-4">
                     <label for="cabang" class="block text-xl font-medium text-gray-700">cabang</label>
                     <select name="cabang" id="cabang"
-                        class="mt-1 capitalize block w-full py-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl"
+                        class="mt-1 capitalize block w-full py-2 border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl"
                         required value="{{ $pelanggan->cabang }}">
                         <option value="telu buyung"
                             @if ($pelanggan->cabang == 'telu buyung') {
@@ -44,22 +44,22 @@
                     </select>
                 </div>
                 <div class="mb-4">
-                    <label for="nama" class="block text-sm font-medium text-gray-700">Tanggal pelanggan</label>
-                    <input type="text" name="nama" id="nama" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required value="{{ $pelanggan->nama }}">
+                    <label for="nama" class="block text-xl font-medium text-gray-700">Tanggal pelanggan</label>
+                    <input type="text" name="nama" id="nama" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl" required value="{{ $pelanggan->nama }}">
                 </div>
                 <div class="mb-4">
-                    <label for="alamat" class="block text-sm font-medium text-gray-700">Total pelanggan</label>
-                    <input type="text" name="alamat" id="alamat" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required value="{{ $pelanggan->alamat }}">
+                    <label for="alamat" class="block text-xl font-medium text-gray-700">Total pelanggan</label>
+                    <input type="text" name="alamat" id="alamat" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl" required value="{{ $pelanggan->alamat }}">
                 </div>
                 <div class="mb-4">
-                    <label for="no_telepon" class="block text-sm font-medium text-gray-700">no_telepon</label>
-                    <input type="number" name="no_telepon" id="no_telepon" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required value="{{ $pelanggan->no_telepon }}">
+                    <label for="no_telepon" class="block text-xl font-medium text-gray-700">no_telepon</label>
+                    <input type="number" name="no_telepon" id="no_telepon" class="mt-1 block w-full p-2 border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl" required value="{{ $pelanggan->no_telepon }}">
                 </div>
                 <div class="mb-4">
                     <label for="kelompok_pelanggan" class="block text-xl font-medium text-gray-700">kelompok
                         pelanggan</label>
                     <select name="kelompok_pelanggan" id="kelompok_pelanggan"
-                        class="mt-1 capitalize block w-full py-2 border-2 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl"
+                        class="mt-1 capitalize block w-full py-2 border-2 border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl"
                         required value="{{ $pelanggan->kelompok_pelanggan }}">
                         <option value="social"
                             @if ($pelanggan->kelompok_pelanggan == 'social') {
@@ -85,11 +85,11 @@
                 </div>
                 <div class="btn flex gap-2">
                     <a href="{{ route('pelanggan') }}"
-                        class="w-full flex justify-center items-center bg-gray-400 text-white p-2 rounded-md hover:bg-gray-700">
+                        class="w-full flex justify-center items-center shadow-md bg-gray-400 text-white p-2 rounded-md hover:bg-gray-700">
                         <button type="submit">kembali</button>
                     </a>
                     <button type="submit"
-                        class="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700">update</button>
+                        class="w-full bg-blue-600 text-white p-2 rounded-md shadow-md hover:bg-blue-700">update</button>
                 </div>
             </form>
         </div>
